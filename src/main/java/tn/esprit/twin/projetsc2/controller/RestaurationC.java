eed1d1fd27eb461aa4c6f54e713442276b53cc07
nour <bouslimi.nourelhouda@esprit.tn>
2025-11-06 20:12:00 +0100

    activite web service rest -swagger

--- src/main/java/tn/esprit/twin/projetsc2/controller/RestaurationC.java
+++ src/main/java/tn/esprit/twin/projetsc2/controller/RestaurationC.java
@@ -1,5 +1,9 @@
 package tn.esprit.twin.projetsc2.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -16,28 +20,39 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/restauration")
+//documentation
+@Tag(name = "Gestion des Restaurations ", description = "Opérations liées aux restaurations")
 public class RestaurationC {
     @Autowired
     RestaurationInterface restaurationInterface;
+
+    @Operation(summary = "Ajouter une nouvelle restauration", description = "Cette opération permet d'ajouter une nouvelle restauration à la base de données.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Restauration créée avec succès"),
+            @ApiResponse(responseCode = "400", description = "Requête invalide")
+    })
 
     @PostMapping("/addRestauration")
     public Restauration addRestauration(@RequestBody Restauration restauration){
         return restaurationInterface.addRestauration(restauration);
     }
 
+    @Operation(summary = "Ajouter plusieurs restaurations", description = "Ajoute une liste de restaurations dans le système")
     @PostMapping("/addListRestauration")
     public List<Restauration> addListRestauration(@RequestBody List<Restauration> restaurations){
         return restaurationInterface.addRestaurations(restaurations);
     }
-
+    @Operation(summary = "Récupérer toutes les restaurations", description = "Retourne la liste de toutes les restaurations")
     @GetMapping("/getAllRestaurations")
     public List<Restauration> getAllRestaurations(){
         return restaurationInterface.retrieveAllRestaurations();
     }
+    @Operation(summary = "Récupérer une restauration par ID")
     @GetMapping("/getRestauration/{idRestauration}")
     public Restauration getRestauration(@PathVariable Long idRestauration){
         return restaurationInterface.retrieveRestauration(idRestauration);
     }
+    @Operation(summary = "Mettre à jour une restauration existante", description = "Met à jour les informations d'une restauration en fonction de son ID.")
     @PutMapping("/updateRestauration/{idRestauration}")
     public Restauration updateRestauration(@RequestBody Restauration restauration, @PathVariable Long idRestauration){
         Restauration r=restaurationInterface.retrieveRestauration(idRestauration);
@@ -51,11 +66,14 @@ public class RestaurationC {
         }
 
     }
+    @Operation(summary = "Supprimer une restauration par ID", description = "Supprime une restauration spécifique en fonction de son ID.")
     @DeleteMapping("/deleteRestauration/{idRestauration}")
     public void deleteRestauration(@PathVariable Long idRestauration){
         restaurationInterface.removeRestauration(idRestauration);
     }
 
+
+@Operation(summary = "Récupérer les restaurants avec un nombre de places maximum supérieur à une valeur donnée et une date de création avant une date spécifiée", description = "Cette opération permet de récupérer une liste de restaurants qui ont un nombre de places maximum supérieur à la valeur spécifiée et dont la chaîne de restauration a été créée avant la date donnée.")
     @GetMapping("/getRestaurantsNbPlacesMaxGreaterThanAndChaineRestaurationDateCreationBefore/{nbPlaces}/{dateCreation}")
     public List<Restauration> getRestaurantsNbPlacesMaxGreaterThanAndChaineRestaurationDateCreationBefore(@PathVariable Long nbPlaces, @PathVariable String dateCreation){
         try {
